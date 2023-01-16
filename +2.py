9d2261e461823c5c3f130c2f09801f0fa7f9db06
yaramoh <39698361+yaramoh@users.noreply.github.com>
2023-01-16 17:42:19 +0200

    Create +2.py

---
+++ +2.py
@@ -0,0 +1,6 @@
+# thing you wanna change and name
+
+name = input('what is your name:')
+change = input('what do you wanna change this year:')
+
+result= print('hey '+name+'your change this year is '+change)
