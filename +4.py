3d88233be0218b01869e013895ae4e7135e6927b
yaramoh <39698361+yaramoh@users.noreply.github.com>
2023-01-18 11:34:02 +0200

    Create +4.py

---
+++ +4.py
@@ -0,0 +1,8 @@
+# marks
+
+
+Marks = int(input('what are your marks:'))
+if(Marks>=60):
+  print('good')
+else:
+  print("you can't take admission to this collage you need " +str(60- Marks)+ ' degress more')
