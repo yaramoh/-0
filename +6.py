450e39cccca2468fc1aef8e1eeb4f3510891774a
LEEZ <39698361+yaramoh@users.noreply.github.com>
2023-01-18 17:06:15 +0200

    Create +6.py

---
+++ +6.py
@@ -0,0 +1,10 @@
+# Simple arthimatic operation on three vars
+
+A=int(input("what is the first number: "))
+B=int(input("what is the second number: "))
+C=int(input("what is the third number: "))
+
+print("the addition is: "+ str(A+B+C))
+print("the multiplication is: "+ str(A*B*C))
+print("the divison is: "+ str(A/B/C))
+print("the module is: "+ str(A%B%C))
