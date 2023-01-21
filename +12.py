6b8207f5a2029ceb7963a2b91c5282113edea572
LEEZ <39698361+yaramoh@users.noreply.github.com>
2023-01-21 22:09:09 +0200

    Create +12.py

---
+++ +12.py
@@ -0,0 +1,11 @@
+#productivity week
+print('finding max productivity from 3, 5,10')
+sat= int(input("what is your productivity in saturday compared : "))
+sun= int(input("what is your productivity in sunday compared : "))
+mon= int(input("what is your productivity in monday compared : "))
+tus= int(input("what is your productivity in tusday compared : "))
+wednsday= int(input("what is your productivity in wednsday compared : "))
+thru= int(input("what is your productivity in thrusday compared : "))
+fri= int(input("what is your productivity in friday compared : "))
+
+print(max(sat,sun,mon,tus,wednsday,thru,fri))
