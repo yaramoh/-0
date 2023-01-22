3723f45204c0766e15418cd336bc32dc8b378c4d
LEEZ <39698361+yaramoh@users.noreply.github.com>
2023-01-22 18:18:19 +0200

    Create +16.py

---
+++ +16.py
@@ -0,0 +1,15 @@
+#sum of numbers in a list
+
+
+#sum of numbers in a list
+
+print("getting sum of numbers in a list")
+
+list=[]
+for i in range(6):
+ ls=list.append( int(input("Enter digit")))
+
+print(list)
+
+list.clear()
+print(list)
